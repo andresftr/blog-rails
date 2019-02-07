010a0f0fe9bbf62f03866c77c4faebec71ebb5fe
AndresFTR <afelipe0113@gmail.com>
2019-02-07 17:42:56 -0500

    add users model with devise gem and add some of bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   
   root 'welcome#index'
 end
